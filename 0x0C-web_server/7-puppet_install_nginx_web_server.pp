4ada5588c4f0b1ae95015e6748754561cbae8608
sayedazp <sayedazp@gmail.com>
2023-09-28 03:10:07 +0300

    task7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+#puppet manifest to install and configure nginx 
+package { 'nginx':
+    ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  replace => true,
+}
+
+file { '/etc/nginx/sites-available/default':
+  content => "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   /var/www/html;
+    index  index.html;
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4&ab_channel=NyanCat;
+    }
+}",
+}
